f73c880ccc3e92483350772834f8dcb70ad7196d
0xsirrush <thaibao@autonomous.nyc>
2019-03-15 17:42:31 +0700

    - UPDATE

--- src/modules/history/History.js
+++ src/modules/history/History.js
@@ -140,7 +140,7 @@ const HistoryItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: stretch;
-  padding: 0px 0px;
+  padding: 10px 0px;
   border-bottom: 1px solid #e4e7f2;
 `;
 
