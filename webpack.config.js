70a47168ea60d5ca9c71fa5e551079cff8a7ce1c
0xsirrush <thaibao@autonomous.nyc>
2019-03-22 15:28:23 +0700

    - use webpack to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|css)$/,
         use: [
           "style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
