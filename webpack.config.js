7074593e969b7c92c6f6173f76337c37d21a7e1d
0xsirrush <thaibao@autonomous.nyc>
2019-03-22 16:27:41 +0700

    - use webpack to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,81 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const stats = {
+  modules: false,
+  children: false,
+  chunks: false,
+};
+
+const appPath = filepath => path.resolve(__dirname, filepath);
 
 module.exports = {
   entry: './src/index.js',
   output: {
+    // path: appPath('../dist'),
     filename: '[name].js?v=[hash]',
     chunkFilename: '[name].chunk.js?v=[hash]',
     publicPath: '/',
-    globalObject: 'this',
+    // globalObject: 'this',
   },
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './build',
-    port: 3000
+    host: 'localhost',
+    watchContentBase: true,
+    stats,
+    port: '3000',
+    disableHostCheck: true,
+    publicPath: '/',
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    hot: true,
   },
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@common': path.resolve(__dirname, 'src/common'),
+      '@assets': appPath('src/assets'),
+      '@common': appPath('src/common'),
     },
     extensions: ['.js', '.jsx', '.css', '.png', '.jpg', '.gif', '.jpeg', '.svg'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['main', 'vendors~main'],
+      minify: false
+        ? {
+          collapseWhitespace: true,
+          preserveLineBreaks: true,
+          removeComments: true,
+        }
+        : null,
+      filename: 'index.html',
+      template: appPath('./public/index.html'),
+      favicon: appPath('./public/favicon.ico'),
+    }),
+    new CopyWebpackPlugin([
+      // relative path is from src
+      {from: './public/manifest.json', to: './'}, // <- your path to manifest
+      {from: './public/img', to: './img'}, // <- your path to manifest
+    ])
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: false},
+          },
+        ],
       },
       {
         test: /\.(png|gif|jpe?g|svg|webp)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
