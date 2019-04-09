2dbeb4f9e470f4cfdcffe2848c4cbc390cc028e0
Keresh <vuong@autonomous.nyc>
2019-04-09 17:27:27 +0700

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -41,16 +41,17 @@ const optimization = {
     cacheGroups: {
       vendor: {
         test: /[\\/]node_modules[\\/]/,
-        name(module) {
-          // get the name. E.g. node_modules/packageName/not/this/part.js
-          // or node_modules/packageName
-          const packageName = module.context.match(
-            /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
-          )[1];
+        // name(module) {
+        //   console.log('====', module);
+        //   // get the name. E.g. node_modules/packageName/not/this/part.js
+        //   // or node_modules/packageName
+        //   const packageName = module.context.match(
+        //     /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+        //   )[1];
 
-          // npm package names are URL-safe, but some servers don't like @ symbols
-          return `npm.${packageName.replace('@', '')}`;
-        },
+        //   // npm package names are URL-safe, but some servers don't like @ symbols
+        //   return `npm.${packageName.replace('@', '')}`;
+        // },
         chunks: 'all'
       },
     },
