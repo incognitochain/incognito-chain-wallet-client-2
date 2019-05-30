a2e826ba049bd987f4da8228033a55b0df797dfa
Keresh <vuong@autonomous.nyc>
2019-05-30 17:08:33 +0700

    Update utils

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,26 +1,17 @@
-import convert from '@src/utils/convert';
 import moment from 'moment';
+import { CONSTANT_COMMONS } from '@src/constants';
 
-const AmountConstantFormat = new Intl.NumberFormat('en-US', {
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
-
-const AmountMiliConstantFormat = new Intl.NumberFormat('en-US');
-
-const AmountTokenFormat = new Intl.NumberFormat('en-US');
-
-const amountMiliConstant = (amount = throw new Error('Amount is required!')) => AmountMiliConstantFormat.format(amount);
-const amountConstant = (amount = throw new Error('Amount is required!')) => AmountConstantFormat.format(convert.toConstant(amount));
-const amountToken = (amount = throw new Error('Amount is required!')) => AmountTokenFormat.format(Number.parseInt(amount) || 0);
-
+const AmountFormat = new Intl.NumberFormat('en-US');
+const amount = (amount = throw new Error('Amount is required!'), tokenSymbol) => {
+  const decision_rate = CONSTANT_COMMONS.DECISION_RATE[tokenSymbol] || 1;
+  const _amount = amount/decision_rate;
+  return Math.max(AmountFormat.format(_amount), 0);
+};
 const formatDateTime = (dateTime, formatPartern) => moment(dateTime).format(formatPartern || 'DD/MM/YYYY - HH:mm:ss');
 const toMiliSecond = (second) => second * 1000;
 
 export default {
-  amountMiliConstant,
-  amountConstant,
-  amountToken,
+  amount,
   formatDateTime,
   toMiliSecond
 };
