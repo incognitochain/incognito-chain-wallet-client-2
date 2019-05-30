d4d79d48d445b2fea816452c94b4a138ee21585b
Keresh <vuong@autonomous.nyc>
2019-05-30 15:07:50 +0700

    Update container component

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { styled } from '@material-ui/styles';
+import bg from '@src/assets/image/incoginito-bg.webp';
 import Header from './Header';
 
 const ContainerStyled = styled('div')(({ theme }) => ({
-  padding: theme.spacing(1)
+  padding: theme.spacing(1),
+  backgroundImage: `url(${bg})`,
+  height: '100%',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'contain',
+  display: 'flex',
+  flexDirection: 'column'
 }));
+
+const Content = styled('div')({
+  minWidth: '100%',
+  flex: 1
+});
 
 const Container =  ({ children }) => {
   return (
     <ContainerStyled>
       <Header />
-      <div>{children}</div>
+      <Content>{children}</Content>
     </ContainerStyled>
   );
 };
