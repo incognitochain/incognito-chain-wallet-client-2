402452b41193785b73e09813e196a800b175cf77
0xkraken <hien.n@autonomous.nyc>
2019-08-07 16:55:33 +0700

    update privacy wasm

--- src/services/Account.js
+++ src/services/Account.js
@@ -197,4 +197,12 @@ export default class Account {
 
     return result;
   }
+
+  /**
+   *
+   * @param {object} accountWallet
+   */
+  static toSerializedAccountObj(accountWallet) {
+    return accountWallet.toSerializedAccountObj();
+  }
 }
